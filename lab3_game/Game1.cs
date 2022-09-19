5f45a62c72fa3752d767c7a0a834cf3d36248e0a
Thanakrit Soomelux <cartoonjr@hotmail.com>
2022-09-19 10:37:21 +0700

    Add project files.

---
+++ lab3_game/Game1.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace lab3_game
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D myTexture;
+
+        Texture2D cloudTexture;
+        Vector2[] scaleCloud;
+        Vector2[] cloudPos;
+        int[] speed;
+
+        Random r = new Random();
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            cloudTexture = Content.Load<Texture2D>("Cloud_sprite");
+
+            cloudPos = new Vector2[4];
+            scaleCloud = new Vector2[4];
+            speed = new int[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                cloudPos[i].Y = r.Next(0, graphics.GraphicsDevice.Viewport.Height - cloudTexture.Height);
+                scaleCloud[i].X = r.Next(1, 3);
+                scaleCloud[i].Y = scaleCloud[i].X;
+                speed[i] = r.Next(3, 7);
+            }
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            for (int i = 0; i < 4; i++)
+            {
+                cloudPos[i].X = cloudPos[i].X + speed[i];
+                if (cloudPos[i].X > graphics.GraphicsDevice.Viewport.Width)
+                {
+                    cloudPos[i].X = 0;
+                    cloudPos[i].Y = r.Next(0, graphics.GraphicsDevice.Viewport.Height - cloudTexture.Height);
+                    scaleCloud[i].X = r.Next(1, 3);
+                    scaleCloud[i].Y = scaleCloud[i].X;
+                }
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(new Color(85, 133, 50));
+            spriteBatch.Begin();
+            //cloud
+            for (int i = 0; i < 4; i++)
+            {
+                spriteBatch.Draw(cloudTexture, cloudPos[i], null, Color.White, 0, Vector2.Zero, scaleCloud[i], 0, 0);
+            }
+            spriteBatch.End();
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
